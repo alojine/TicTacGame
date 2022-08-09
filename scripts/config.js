c762a89bd4d17a74ac22a2bbfc6b1dba39a84ce5
alojine <76651648+alojine@users.noreply.github.com>
2022-08-09 18:09:04 +0300

    config.js and ap.js
    logic for entering names and saving, closing data

--- scripts/config.js
+++ scripts/config.js
@@ -1,9 +1,37 @@
-function openPlayerConfig() {
+// Opens player configuration after Edit button
+function openPlayerConfig(event) {
+    editedPlayer = +event.target.dataset.playerid; // '1' to 1
     playerConfigOverlayElement.style.display = 'block';
     backdropElement.style.display = 'block';
 }
 
+// Closes player configuration after Close button or backdrop click
 function closePlayerConfig() {
     playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutputElement.textContent = '';
+    formElement.firstElementChild.lastElementChild.value = ''; // Reseting input
 }
+
+// Saving player name
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayername = formData.get('playername').trim();
+
+    if(!enteredPlayername) {
+        event.target.firstElementChild.classList.add('error');
+        errorsOutputElement.textContent = 'Please enter a valid playername!';
+        return;
+    }
+
+    const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
+    updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+
+    players[editedPlayer - 1].name = enteredPlayername;
+
+    closePlayerConfig();    // Manually calling to close
+}
+
+
