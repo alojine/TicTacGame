021a9ef343e0cf274e6a6dd29779c48a92126a6b
alojine <76651648+alojine@users.noreply.github.com>
2022-08-10 17:54:27 +0300

    game logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
     {
@@ -16,10 +17,14 @@ const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-errors');
+const gameAreaElement = document.getElementById('active-game');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
+const gameFieldElements = document.querySelectorAll('#game-board li');
+
+const startNewGameBtnElement = document.getElementById('start-game-btn');
 
 // Event listeners
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
@@ -29,3 +34,9 @@ cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
 
 formElement.addEventListener('submit', savePlayerConfig);
+
+startNewGameBtnElement.addEventListener('click', startNewGame);
+
+for (const gameFieldElement of gameFieldElements) {
+    gameFieldElement.addEventListener('click', selectGameField(activePlayer));
+}
